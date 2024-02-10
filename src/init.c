49bbcb46f0097eb8066d0659436548d4d6de8de1
fwatanab <fwatanab@student.42tokyo.jp>
2024-02-10 23:20:46 +0900

    t_camera init / playerの初期化

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 19:21:55 by fwatanab          #+#    #+#             */
-/*   Updated: 2024/02/04 20:37:49 by fwatanab         ###   ########.fr       */
+/*   Updated: 2024/02/10 23:17:21 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,44 @@ t_vars	*vars_init()
 	if (!vars)
 		error("Error: Malloc failure.");
 	return (vars);
+}
+
+int	search_player(char **map, int flag)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (map[y][x] == 'N' || map[y][x] == 'S' || map[y][x] == 'W' || map[y][x] == 'E')
+			{
+				if (flag)
+					return (x);
+				return (y);
+			}
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
+t_camera	*init_player(t_map *conf)
+{
+	t_camera	*player;
+
+	player = malloc(sizeof(t_camera));
+	if (!player)
+		error("Error: Malloc failure.");
+	player->pos_x = search_player(conf->map, 1);
+	player->pos_y = search_player(conf->map, 0);
+	player->dir_x = -1;
+	player->dir_y = -1;
+	player->plane_x = 0;
+	player->plane_y = 1;
+	return (player);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 19:06:09 by fwatanab          #+#    #+#             */
-/*   Updated: 2024/02/07 01:37:24 by fwatanab         ###   ########.fr       */
+/*   Updated: 2024/02/10 23:14:06 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int	main(int argc, char **argv)
 {
-//	t_vars	vars;
-	t_map	*conf;
+	t_vars		vars;
+	t_map		*conf;
+	t_camera	*player;
 
 	if (argc != 2)
 		error("Error: Invalid number of arguments.");
 	conf = parser(argv);
-	parser_print(conf);
+	player = init_player(conf);
+	vars.mlx = mlx_init();
+	vars.mlx_win = mlx_new_window(vars.mlx, 1400, 700, "cub3d");
+	mlx_loop(vars.mlx);
 	map_free(conf);
-//	vars.mlx = mlx_init();
-//	vars.mlx_win = mlx_new_window(vars.mlx, 1400, 700, "cub3d");
-//	mlx_loop(vars.mlx);
-	system("leaks -q cub3d");
 	return (0);
 }
 
