41cf99a2f6ddea8eccf4c5a9f523f011d65d2c6b
fwatanab <fwatanab@student.42tokyo.jp>
2024-02-12 21:03:29 +0900

    draw_textureのテスト中

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 19:06:09 by fwatanab          #+#    #+#             */
-/*   Updated: 2024/02/12 16:37:52 by fwatanab         ###   ########.fr       */
+/*   Updated: 2024/02/12 17:31:34 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int argc, char **argv)
 	t_map		*conf;
 	t_camera	*player;
 	t_ray		*ray;
+	t_texture	tex;
 
 	if (argc != 2)
 		error("Error: Invalid number of arguments.");
@@ -27,7 +28,10 @@ int	main(int argc, char **argv)
 	calculate_ray_direction(ray, player);
 	perform_dda(player, ray, conf);
 	vars.mlx = mlx_init();
-	vars.mlx_win = mlx_new_window(vars.mlx, 1400, 700, "cub3d");
+	vars.mlx_win = mlx_new_window(vars.mlx, WIN_WIDTH, WIN_HEIGHT, "cub3d");
+	tex = load_texture(vars, conf);
+	double	wall_dist = 20; //テスト用
+	draw_wall(vars, wall_dist);
 	mlx_loop(vars.mlx);
 	map_free(conf);
 	free(player);
