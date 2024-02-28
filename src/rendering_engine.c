2673bbf5236241fb92af820c561af9daa08daff7
fwatanab <fwatanab@student.42tokyo.jp>
2024-02-28 18:59:45 +0900

    ddaアルゴリズムの実装

--- src/rendering_engine.c
+++ src/rendering_engine.c
@@ -3,10 +3,15 @@
 void	calculate_ray_direction(t_ray *ray, t_camera *player, int x)
 {
 	double	camera_x;
+	double	red_to_deg;
 
 	camera_x = 2 * x / (double)WIN_WIDTH -1;
 	ray->dir_x = player->dir_x + player->plane_x * camera_x;
 	ray->dir_y = player->dir_y + player->plane_y * camera_x;
+	red_to_deg = 180.0 / M_PI;
+	ray->ray_angle = player->dir_angle + atan2(ray->dir_y, ray->dir_x) * red_to_deg;
+	if (ray->ray_angle < 0)
+		ray->ray_angle += 360;
 }
 
 void	execute_dda(t_ray *ray, t_map *conf)
@@ -28,7 +33,7 @@ void	execute_dda(t_ray *ray, t_map *conf)
 			ray->map_y += ray->step_y;
 			ray->side = 1;
 		}
-		if (conf->map[ray->map_x][ray->map_y] == 1)
+		if (conf->map[ray->map_x][ray->map_y] == '1')
 			hit = 1;
 	}
 }
@@ -37,25 +42,28 @@ void	perform_dda(t_camera *player, t_ray *ray, t_map *conf)
 {
 	ray->map_x = (int)player->pos_x;
 	ray->map_y = (int)player->pos_y;
-	ray->delta_dist_x = fabs(1 / player->dir_x);
-	ray->delta_dist_y = fabs(1 / player->dir_y);
-	if (player->dir_x < 0)
+	ray->delta_dist_x = (ray->dir_x == 0) ? 1e30 : fabs(1 / ray->dir_x);
+	ray->delta_dist_y = (ray->dir_y == 0) ? 1e30: fabs(1 / ray->dir_y);
+	if (ray->dir_x < 0)
 	{
 		ray->step_x = -1;
 		ray->side_dist_x = (player->pos_x - ray->map_x) * ray->delta_dist_x;
-	} else
+	}
+	else
 	{
 		ray->step_x= 1;
 		ray->side_dist_x = (ray->map_x + 1.0 - player->pos_x) * ray->delta_dist_x;
 	}
-	if (player->dir_y < 0)
+	if (ray->dir_y < 0)
 	{
 		ray->step_y = -1;
 		ray->side_dist_y = (player->pos_y - ray->map_y) * ray->delta_dist_y;
-	} else
+	}
+	else
 	{
 		ray->step_y = 1;
 		ray->side_dist_y = (ray->map_y + 1.0 - player->pos_y) * ray->delta_dist_y;
 	}
 	execute_dda(ray, conf);
+	printf("map_x: %d, map_y: %d, side_dist_x: %f, side_dist_y: %f\n", ray->map_x, ray->map_y, ray->side_dist_x, ray->side_dist_y);
 }
