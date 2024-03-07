cc909c3302ca302b8402e6ad4f12397fac8b3226
fwatanab <fwatanab@student.42tokyo.jp>
2024-03-07 18:18:34 +0900

    1ピクセルずつ描画していたのを一括で描画するように修正 / 高速化

--- src/drawing.c
+++ src/drawing.c
@@ -6,36 +6,11 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 16:48:47 by fwatanab          #+#    #+#             */
-/*   Updated: 2024/02/28 18:40:29 by fwatanab         ###   ########.fr       */
+/*   Updated: 2024/03/07 18:14:10 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
-
-//static void	draw_texture(t_vars vars, int start, int end, int color)
-//{
-//	while (start <= end)
-//	{
-//		mlx_pixel_put(vars.mlx, vars.mlx_win, WIN_HEIGHT / 2, start, color);
-//		start++;
-//	}
-//}
-//
-//void	get_wall_height(t_vars vars, double wall_dist)
-//{
-//	double	wall_height;
-//	int		start;
-//	int		end;
-//
-//	wall_height = WIN_HEIGHT / wall_dist;
-//	start = -wall_dist / 2 + WIN_HEIGHT / 2;
-//	if (start < 0)
-//		start = 0;
-//	end = wall_height = WIN_HEIGHT / 2 + wall_dist / 2;
-//	if (end >= WIN_HEIGHT)
-//		end = WIN_HEIGHT -1;
-//	draw_texture(vars, start, end, 0x00FFFFFF);
-//}
 
 double	get_wall_dist(t_ray *ray, t_camera *player)
 {
@@ -49,11 +24,12 @@ double	get_wall_dist(t_ray *ray, t_camera *player)
 	return (wall_dist);
 }
 
-void	draw_wall(t_vars vars, t_ray *ray, int x, double wall_dist)
+void	draw_wall(t_vars *vars, t_ray *ray, int x, double wall_dist, char *buf)
 {
 	int		wall_height;
 	int		draw_start;
 	int		draw_end;
+	double	wall_x;
 
 	if (wall_dist <= 0.0)
 		wall_dist = 0.1;
@@ -64,6 +40,11 @@ void	draw_wall(t_vars vars, t_ray *ray, int x, double wall_dist)
 	draw_end = wall_height / 2 + WIN_HEIGHT / 2;
 	if (draw_end >= WIN_HEIGHT)
 		draw_end = WIN_HEIGHT - 1;
+	if (ray->side == 0)
+		wall_x = vars->player->pos_y + wall_dist * ray->dir_y;
+	else
+		wall_x = vars->player->pos_x + wall_dist * ray->dir_x;
+	wall_x -= floor(wall_x);
 	int	y;
 
 	y = draw_start;
@@ -71,19 +52,37 @@ void	draw_wall(t_vars vars, t_ray *ray, int x, double wall_dist)
 	{
 		int d = (y - draw_start) * 256;
 		int	tex_y = ((d * TEX_HEIGHT) / WIN_HEIGHT) / 256;
-		double	wall_x;
-		if (ray->side == 0)
-			wall_x = vars.player->pos_y + wall_dist * ray->dir_y;
-		else
-			wall_x = vars.player->pos_x + wall_dist * ray->dir_x;
-		wall_x -= floor(wall_x);
 		int	tex_x = (int)(wall_x * (double)TEX_WIDTH);
-		if (ray->side == 0 && ray->dir_x > 0)
+		if ((ray->side == 0 && ray->dir_x > 0)
+			|| (ray->side == 1 && ray->dir_y > 0))
 			tex_x = TEX_WIDTH - tex_x - 1;
-		if (ray->side == 1 && ray->dir_y < 0)
-			tex_x = TEX_WIDTH - tex_x - 1;
-		int	color = ((int *)vars.tex.no.addr)[tex_y * TEX_WIDTH + tex_x];
-		mlx_pixel_put(vars.mlx, vars.mlx_win, x, y, color);
+		int	color = ((int *)vars->tex.no.addr)[tex_y * TEX_WIDTH + tex_x];
+		*(int *)(buf + (x + y * WIN_WIDTH) * 4) = color;
 		y++;
 	}
+}
+
+void	draw_floor_and_ceiling(t_vars *vars, t_tex_img *buf)
+{
+	int			x;
+	int			y;
+	int			f_color;
+	int			c_color;
+	int			color;
+
+	f_color = (vars->conf->f->r << 16) + (vars->conf->f->g << 8) + vars->conf->f->b;
+	c_color = (vars->conf->c->r << 16) + (vars->conf->c->g << 8) + vars->conf->c->b;
+	y = 0;
+	while (y < WIN_HEIGHT)
+	{
+		x = 0;
+		while (x < WIN_WIDTH)
+		{
+			color = (y < WIN_HEIGHT / 2) ? c_color : f_color;
+			*(int *)(buf->addr + (x + y * WIN_WIDTH) * (buf->pixel / 8)) = color;
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(vars->mlx, vars->mlx_win, buf->img, 0, 0);
 }
