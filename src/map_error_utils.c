e0da8838fd6d29befcaf23ede06d18d2233ff295
stakimot <kiiro18.sh@gmail.com>
2024-04-15 20:09:48 +0900

    map_error_utils.c

--- src/map_error_utils.c
+++ src/map_error_utils.c
@@ -6,11 +6,11 @@
 /*   By: stakimot <stakimot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 16:22:54 by stakimot          #+#    #+#             */
-/*   Updated: 2024/03/30 16:23:38 by stakimot         ###   ########.fr       */
+/*   Updated: 2024/04/15 20:08:47 by stakimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "../inc/cub3d.h"
 
 void	flg_check(int *l, int *l_flg, int cnt)
 {
